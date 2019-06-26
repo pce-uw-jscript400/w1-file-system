c6810b9dc1dc3cb7e0060f16c3e3f42a72d37ef0
Michael Munsey <munseym@uw.edu>
2019-06-25 21:10:41 -0700

    in class exercises

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello, Node!')
+console.log('Hello, Node!');
+
+const fs = require('fs');
+
+const contents = fs.readFileSync('./data/pets.sample.json', 'utf-8');
+const pets = JSON.parse(contents);
+console.log(pets);
+
+const contents2 = fs.readFile('./data/pet.ssample.json', 'utf-8', (err, data) => {
+    const pets2 = JSON.parse(data);
+    console.log(pets2);
+});
+
+pets.push({ name: 'Oliver', kind: 'cat', age:2});
+const stringy = JSON.stringify(pets);
+
+fs.writeFileSync('./data/pets.sample.json', stringy);
