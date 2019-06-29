6ca32cdd44f5017cd3a03edbe24a430e7a861ba1
Tom Workman <tworkman512@gmail.com>
2019-06-28 22:28:35 -0500

    refactor(PetsJSON): Adds some nice Node globals and refactors read and create functions

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
 const fs = require("fs");
-const contents = fs.readFileSync("./data/pets.json", "utf-8");
-const pets = JSON.parse(contents);
+const path = require("path");
+const petsFile = path.join(__dirname, "data", "pets.json");
+// const contents = fs.readFileSync(petsFile, "utf-8");
+// const pets = JSON.parse(contents);
 
 // Read contents
-const read = pets => pets;
-console.log("### This reads files ->", read(pets));
-
-// Write to contents
-const create = pet => {
-  pets.push(pet);
-  const upsertNewPet = JSON.stringify(pets);
-  fs.writeFileSync("./data/pets.json", upsertNewPet);
+const read = () => {
+  const contents = fs.readFileSync(petsFile, "utf-8");
+  const pets = JSON.parse(contents);
+  return pets;
 };
 
-// Please feel free to enter new pet values below
-console.log(
-  "### This creates a new pets record ->",
-  create({ name: "Rosco", kind: "dog", age: 7 })
-);
+// Write to contents
+const create = (name, kind, age) => {
+  const contents = fs.readFileSync(petsFile, "utf-8");
+  const pets = JSON.parse(contents);
+  const newPet = {
+    name,
+    kind,
+    age
+  };
+  pets.push(newPet);
+  const petString = JSON.stringify(pets);
+  fs.writeFileSync(petsFile, petString);
+  return read();
+};
+
+console.log(read());
+// [ { name: 'Meowser', kind: 'cat', age: 3 } ]
+
+console.log(create("Duchess", "bird", 2));
+// [
+//   { name: 'Meowser', kind: 'cat', age: 3 },
+//   { name: 'Duchess', kind: 'bird', age: 2 }
+// ]
+
+console.log(create("Duchess", "bird", 2));
+// "Duchess" is already a pet!
+
+console.log(remove("Snoopy"));
+// No pet found by the name of "Snoopy"
+
+console.log(remove("Duchess"));
+// { name: 'Duchess', kind: 'bird', age: 2 }
+
+console.log(read());
+// [ { name: 'Meowser', kind: 'cat', age: 3 } ]
+
+console.log(update("Duchess", "bird", 3));
+// No pet found by the name of "Duchess"
+
+console.log(update("Meowser", "cat", 4));
+// { name: 'Meowser', kind: 'cat', age: 4 }
+
+console.log(read());
+// [ { name: 'Meowser', kind: 'cat', age: 4 } ]
+
+console.log(update("Meowser", "cat", 3));
+// { name: 'Meowser', kind: 'cat', age: 3 }
