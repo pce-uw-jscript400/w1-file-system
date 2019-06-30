38ac26ff0924646cd6789508b513a73748785257
Michael Munsey <munseym@uw.edu>
2019-06-29 17:49:44 -0700

    Create working

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
+var _ = require('underscore');
+
 console.log('Hello, Node!');
 
 const fs = require('fs');
 
-const contents = fs.readFileSync('./data/pets.sample.json', 'utf-8');
-const pets = JSON.parse(contents);
-console.log(pets);
+// const contents = fs.readFileSync('./data/pets.sample.json', 'utf-8');
+// const pets = JSON.parse(contents);
+// console.log(pets);
 
-// const contents2 = fs.readFile('./data/pet.ssample.json', 'utf-8', (err, data) => {
+// async read
+// const contents2 = fs.readFile('./data/pet.sample.json', 'utf-8', (err, data) => {
 //     const pets2 = JSON.parse(data);
 //     console.log(pets2);
 // });
 
-pets.push({ name: 'Oliver', kind: 'cat', age:2});
-const stringy = JSON.stringify(pets);
-
-fs.writeFileSync('./data/pets.sample.json', stringy);
-
-// PAIR EXERCISE
+//console.log(read())
+// [ { name: 'Meowser', kind: 'cat', age: 3 } ]
 function read(){
-    const pets = fs.readFileSync('./data/pets.sample.json', 'utf-8');
+    const pets = fs.readFileSync('./data/pets.json', 'utf-8');
     const p = JSON.parse(pets);
     return p;
 }
 
-console.log(read())
+console.log('\nread');
+console.log(read());
 
-console.log('***********');
+// console.log(create('Duchess', 'bird', 2))
+// [
+//   { name: 'Meowser', kind: 'cat', age: 3 },
+//   { name: 'Duchess', kind: 'bird', age: 2 }
+// ]
+// console.log(create('Duchess', 'bird', 2))
+// "Duchess" is already a pet!
+
 function create(name, kind, age){
-    const pets = fs.readFileSync('./data/pets.sample.json', 'utf-8');
+    const pets = fs.readFileSync('./data/pets.json', 'utf-8');
     const p = JSON.parse(pets);
     const temp = {name, kind, age};
 
-    //index doesn't actually work here, try something else
-    console.log(p.indexOf(temp));
-    if(p.indexOf(temp) > 0){
+    if(_.findWhere(p, temp)){
         return temp.name + ' is already a pet!'
     }else{
         p.push(temp);
+        fs.writeFileSync('./data/pets.json', JSON.stringify(p));
         return p;  
     }  
 }
 
-console.log(create('Duchess', 'bird', 2))
+console.log('\ncreate');
+console.log(create('Duchess', 'bird', 2));
 
+console.log('\ncreate');
 console.log(create('Duchess', 'bird', 2));
