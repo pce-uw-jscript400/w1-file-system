040c1d9b9c4f524214cc0cc302424268beec10bd
Michael Munsey <munseym@uw.edu>
2019-06-30 21:32:08 -0700

    rearrange tests

--- index.test.js
+++ index.test.js
@@ -11,46 +11,27 @@ test('read', () => {
 });
 
 test('create', () => {
-    const expected = [{ name: 'Meowser', kind: 'cat', age: 3 }, { name: 'Duchess', kind: 'bird', age: 2 }];
+    let expected = [{ name: 'Meowser', kind: 'cat', age: 3 }, { name: 'Duchess', kind: 'bird', age: 2 }];
     expect(create('Duchess', 'bird', 2)).toMatchObject(expected);
-});
-
-test('createDuplicate', () => {
-    create('Duchess', 'bird', 2);
-    const expected = "\"Duchess\" is already a pet!";
+    expected = "\"Duchess\" is already a pet!";
     expect(create('Duchess', 'bird', 2)).toBe(expected);
 });
 
-test('removeNonexistant', () => {
+test('remove', () => {
     create('Duchess', 'bird', 2);
-    const expected = "No pet found by the name of \"Snoopy\"";
+    let expected = "No pet found by the name of \"Snoopy\"";
     expect(remove('Snoopy')).toBe(expected);
-});
-
-test('removeExistant', () => {
-    create('Duchess', 'bird', 2);
-    const expected = { name: 'Duchess', kind: 'bird', age: 2 };
+    expected = { name: 'Duchess', kind: 'bird', age: 2 };
     expect(remove('Duchess')).toMatchObject(expected);
+    expected = [{ name: 'Meowser', kind: 'cat', age: 3 }];
+    expect(read()).toMatchObject(expected);
 });
 
-test('updateNonexistant', () => {
-    create('Duchess', 'bird', 2);
-    remove('Duchess');
-    const expected = "No pet found by the name of \"Duchess\"";
+test('update', () => {
+    let expected = "No pet found by the name of \"Duchess\"";
     expect(update('Duchess', 'bird', 3)).toBe(expected);
-});
-
-test('updateExistant', () => {
-    create('Duchess', 'bird', 2);
-    remove('Duchess');
-    const expected = { name: 'Meowser', kind: 'cat', age: 4 }
+    expected = { name: 'Meowser', kind: 'cat', age: 4 }
     expect(update('Meowser', 'cat', 4)).toMatchObject(expected);
-});
-
-test('readAfterUpdate', () => {
-    create('Duchess', 'bird', 2);
-    remove('Duchess');
-    update('Meowser', 'cat', 4);
-    const expected = [{ name: 'Meowser', kind: 'cat', age: 4 }];
+    expected = [{ name: 'Meowser', kind: 'cat', age: 4 }];
     expect(read()).toMatchObject(expected);
 });
