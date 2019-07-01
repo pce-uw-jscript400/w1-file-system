605f588c38631aa1dac249c128aefae85022aadb
pjkiefer <pjkiefer@uw.edu>
2019-06-30 17:53:22 -0700

    saving code to git

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello, Node!')
+const fs = require('fs');
+const tools = require('./fileHandler');
+
+console.log(tools.read());
+console.log(tools.create('Duchess','bird',2));
+console.log(tools.create('Duchess','bird',2));
+console.log(tools.remove('Snoopy'));
+console.log(tools.remove('Duchess'));
+console.log(tools.read());
+console.log(tools.update('Duchess','bird', 3));
+console.log(tools.update('Meowser','cat', 4));
+console.log(tools.read());
+console.log(tools.update('Meowser','cat', 3));
+
