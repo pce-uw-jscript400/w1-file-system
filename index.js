f0aaa7f2688ddd72499724bdc4f479e819555c2f
Wes Reid <bwreid@gmail.com>
2019-07-01 20:51:01 -0700

    Class work

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello, Node!')
+const fs = require('fs')
+// read in the contents of the file
+const contents = fs.readFileSync('./data/pets.json', 'utf-8')
+// parsing the contents so that it is a javascript array
+// (in contrast to JSON)
+const pets = JSON.parse(contents)
+// push in another pet
+pets.push({ name: "Oliver", kind: "Cat", age: 2 })
+// transform back into a string
+const stringy = JSON.stringify(pets)
+// write the entire array back to the file
+fs.writeFileSync('./data/pets.json', stringy)
