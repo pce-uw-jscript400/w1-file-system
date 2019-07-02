69383effab70b7d3cf7daea28632bb5804c6b9be
Tom Workman <tworkman512@gmail.com>
2019-07-01 19:34:50 -0500

    feature(PetJSON): start over and gets utility, read, create and creact checker functionality working

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 const fs = require("fs");
 const path = require("path");
 const petsFile = path.join(__dirname, "data", "pets.json");
-// const contents = fs.readFileSync(petsFile, "utf-8");
-// const pets = JSON.parse(contents);
+const contents = fs.readFileSync(petsFile, "utf-8");
+const pets = JSON.parse(contents);
+
+// Utility pet checker
+const runPetSearch = name => {
+  let isPetRecord = pets.some(record => record.name === name);
+  // This console.log is for testing only
+  console.log(`Does pet ${name} exist?`, isPetRecord);
+  return isPetRecord;
+};
 
 // Read contents
 const read = () => {
-  const contents = fs.readFileSync(petsFile, "utf-8");
-  const pets = JSON.parse(contents);
   return pets;
 };
 
 // Write to contents
 const create = (name, kind, age) => {
-  const contents = fs.readFileSync(petsFile, "utf-8");
-  const pets = JSON.parse(contents);
-  const newPet = {
+  const newPetRecord = {
     name,
     kind,
     age
   };
-  pets.push(newPet);
-  const petString = JSON.stringify(pets);
-  fs.writeFileSync(petsFile, petString);
-  return read();
+
+  if (!runPetSearch(name)) {
+    pets.push(newPetRecord);
+    const petString = JSON.stringify(pets);
+    fs.writeFileSync(petsFile, petString);
+    return read();
+  }
+  return `${name} is already a pet!`;
+};
+
+const remove = () => {};
+
+// Remove from contents
+const remove = name => {
+  const contents = fs.readFileSync(petsFile, "utf-8");
+  const pets = JSON.parse(contents);
+  let removePet = pets.filter(pet => pet.name === name);
+
+  if (checkForPet(name)) {
+    const updatedPetList = pets.filter(pet => pet.name !== name);
+    const removedPetString = JSON.stringify(updatedPetList);
+    fs.writeFileSync(petsFile, removedPetString);
+    return removePet;
+  }
+  return `We were not able to remove the name ${name} from our list of pets. Please try again.`;
 };
 
 console.log(read());
@@ -38,23 +63,23 @@ console.log(create("Duchess", "bird", 2));
 console.log(create("Duchess", "bird", 2));
 // "Duchess" is already a pet!
 
-console.log(remove("Snoopy"));
-// No pet found by the name of "Snoopy"
+// console.log(remove("Snoopy"));
+// // No pet found by the name of "Snoopy"
 
-console.log(remove("Duchess"));
-// { name: 'Duchess', kind: 'bird', age: 2 }
+// console.log(remove("Duchess"));
+// // { name: 'Duchess', kind: 'bird', age: 2 }
 
-console.log(read());
-// [ { name: 'Meowser', kind: 'cat', age: 3 } ]
+// console.log(read());
+// // [ { name: 'Meowser', kind: 'cat', age: 3 } ]
 
-console.log(update("Duchess", "bird", 3));
-// No pet found by the name of "Duchess"
+// console.log(update("Duchess", "bird", 3));
+// // No pet found by the name of "Duchess"
 
-console.log(update("Meowser", "cat", 4));
-// { name: 'Meowser', kind: 'cat', age: 4 }
+// console.log(update("Meowser", "cat", 4));
+// // { name: 'Meowser', kind: 'cat', age: 4 }
 
-console.log(read());
-// [ { name: 'Meowser', kind: 'cat', age: 4 } ]
+// console.log(read());
+// // [ { name: 'Meowser', kind: 'cat', age: 4 } ]
 
-console.log(update("Meowser", "cat", 3));
-// { name: 'Meowser', kind: 'cat', age: 3 }
+// console.log(update("Meowser", "cat", 3));
+// // { name: 'Meowser', kind: 'cat', age: 3 }
