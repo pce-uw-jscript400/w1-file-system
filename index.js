a4b3a7c4dbee0e619758bd1177aaf184ea2b7e65
Shahab13 <sam.autom@gmail.com>
2019-07-02 11:20:25 -0700

    just started

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello, Node!')
+//console.log('Hello, Node!')
+
+const fs = require("fs");
+let json;
+const contents = fs.readFileSync("./data/pets.json", "utf-8");
+json = JSON.parse(contents);
+let pet = {
+  name: "Duchess",
+  kind: "bird",
+  age: 2
+};
+console.log(pet);
+json.push(pet);
+let stringy = JSON.stringify(json);
+fs.writeFileSync("./data/pets.json", stringy);
