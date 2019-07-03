b1f6368f23060ab24e9fa201dd92aa908212d820
Michael S. Hoffman <mikeshoffman@gmail.com>
2019-07-02 23:10:30 -0700

    Implement update, delete

--- index.js
+++ index.js
@@ -8,36 +8,43 @@ const read = function (){
 }
 
 const create = function (name, kind, age){
-  // read in the contents of the file
-  const contents = fs.readFileSync(petsFile, 'utf-8')
-  // parse contents so it's a JS array (we could just call our read function)
-  const pets = JSON.parse(contents)
-
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-
-  if ( !pets.find(pet => {pet.name === name} ) ) {
-    // push in another pet
+  const pets = read()
+  if (pets.find(pet => {return pet.name === name})) {
+    return `"${name}" is already a pet!`
+  } else {
     pets.push({name: name, kind: kind, age: age})
-    // transform back into a string
     const stringy = JSON.stringify(pets)
-    // write entire array to file
     fs.writeFileSync(petsFile, stringy)
     return pets
-  } else {
-    console.log(`${name} is already a pet!`)
   }
 }
 
-const update = function(){
-// findIndex
+const update = function(name, kind, age){
+  const pets = read()
+  const index = pets.findIndex(pet => pet.name === name)
+  if (index === -1) {
+      return `No pet found by the name of "${name}"`
+  } else {
+      const updatedPet = {name: name, kind: kind, age: age}
+      pets.splice(index, 1, updatedPet)
+      const stringy = JSON.stringify(pets)
+      fs.writeFileSync(petsFile, stringy)
+      return updatedPet
+  }
 }
 
-const remove = function(){
-  
+const remove = function(name){
+  const pets = read()
+  const index = pets.findIndex(pet => pet.name === name)
+  if (index === -1) {
+      return `No pet found by the name of "${name}"`
+  } else {
+      const removed = pets.splice(index, 1)[0]
+      const stringy = JSON.stringify(pets)
+      fs.writeFileSync(petsFile, stringy)
+      return removed
+  }
 }
-
-
-
 
 console.log(read())
 // [ { name: 'Meowser', kind: 'cat', age: 3 } ]
@@ -51,77 +58,23 @@ console.log(create('Duchess', 'bird', 2))
 console.log(create('Duchess', 'bird', 2))
 // "Duchess" is already a pet!
 
-// console.log(remove('Snoopy'))
+console.log(remove('Snoopy'))
 // No pet found by the name of "Snoopy"
 
-// console.log(remove('Duchess'))
+console.log(remove('Duchess'))
 // { name: 'Duchess', kind: 'bird', age: 2 }
 
-// console.log(read())
+console.log(read())
 // [ { name: 'Meowser', kind: 'cat', age: 3 } ]
 
-// console.log(update('Duchess', 'bird', 3))
+console.log(update('Duchess', 'bird', 3))
 // No pet found by the name of "Duchess"
 
-// console.log(update('Meowser', 'cat', 4))
+console.log(update('Meowser', 'cat', 4))
 // { name: 'Meowser', kind: 'cat', age: 4 }
 
-// console.log(read())
+console.log(read())
 // [ { name: 'Meowser', kind: 'cat', age: 4 } ]
 
-// console.log(update('Meowser', 'cat', 3))
+console.log(update('Meowser', 'cat', 3))
 // { name: 'Meowser', kind: 'cat', age: 3 }
-
-
-
-
-
-
-
-
-// const fs = require('fs')
-// // read in the contents of the file
-// const contents = fs.readFileSync('./data/pets.json', 'utf-8')
-// // parse contents so it's a JS array
-// const pets = JSON.parse(contents)
-// // push in another pet
-// pets.push({name: "Oliver", kind: "Cat", age: 2})
-// // transform back into a string
-// const stringy = JSON.stringify(pets)
-// // write entire array to file
-// fs.writeFileSync('/data/pets.json', stringy)
-
-
-
-
-// console.log('Hello, Node!')
-
-// const fs = require('fs')
-// const contents = fs.readFileSync('data/pets.json')
-
-// const json = JSON.parse(contents)
-// console.log(json)
-// output:
-// [ { name: 'Meowser', kind: 'cat', age: 3 } ]
-
-// console.log(json[0].name)
-// output:
-// Meowser
-
-// const contents = fs.readFileSync('package.json', 'utf-8')
-// const json = JSON.parse(contents)
-// console.log(json)
-
-
-// const contents = fs.readFile('./package.json', 'utf-8', (err, data) => {
-//   const json = JSON.parse(contents)
-//   console.log(json)
-// })
-
-
-// console.log(contents)
-
-// console.log(typeof contents) // object
-
-
-// console.log(typeof contents) // object
